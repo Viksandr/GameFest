01d534e1b5579b61e7b36f25a7555d38fa854881
Unknown <iviktor2614@gmail.com>
2017-04-20 17:14:23 +0300

    create homepage

---
+++ application/controllers/SiteController.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\controllers;
+class SiteController extends \yii\web\Controller
+{
+        public function actionIndex()
+        {
+            return $this->render('homePage');
+        }
+}
