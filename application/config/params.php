02b824103c8a51527de2857ecd48b512e7ff4804
Unknown <iviktor2614@gmail.com>
2017-05-22 18:10:09 +0300

    Tournaments page

--- application/config/params.php
+++ application/config/params.php
@@ -2,4 +2,5 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'tournaments' => require(__DIR__ . '/tournaments.php')
 ];
---
+++ application/config/tournaments.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    [
+        'label' => '1 vs 1 Турниры',
+        'items' => ['Dota 2', 'LoL', 'HearthStone', 'Mortal Kombat 9', 'Tekken', 'D.O.A'],
+    ],
+    [
+        'label' => '5 vs 5 Турниры',
+        'items' => ['Dota 2', 'LoL', 'CS:GO']
+    ],
+    /*
+    'cosplay' => [
+        'label' => 'Косплей',
+        'items' => [
+            'single-defile' => [
+                'label' => 'Одиночное дефиле'
+            ],
+            'group-defile' => [
+                'label' => 'Групповое дефиле'
+            ],
+            'sketch' => [
+                'label' => 'Сценка'
+            ],
+            'karaoke' => [
+                'label' => 'Караоке'
+            ]
+        ]
+    ],
+    'k-pop' => [
+        'label' => 'K-Pop',
+        'items' => []
+    ]
+    */
+];
